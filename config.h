189efe029febfada85503cd75f9924a1cbb26721
SoopaKhell <SoopaKhell@users.noreply.github.com>
2021-03-12 17:01:49 -0500

    change font

--- config.h
+++ config.h
@@ -14,7 +14,7 @@ static const unsigned int gappx     = 20;        /* gaps between windows */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Monaco:size=12" };
+static const char *fonts[]          = { "Gohu Gohufont:size=14" };
 static const char dmenufont[]       = "Monaco:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
